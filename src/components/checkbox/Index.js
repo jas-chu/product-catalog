4df24a5c5d9b1de6b13da2e6c327d7a2ebb8f2bf
Jasmina Sella Faena <jasminasf@gmail.com>
2022-09-12 22:09:47 +0200

    using index.js to import

--- src/components/checkbox/Index.js
+++ src/components/checkbox/Index.js
@@ -4,9 +4,10 @@ import './Checkbox.css';
 function Checkbox({ title, name }) {
   return (
     <div>
+      <p className="Input-title">{title}</p>
       <label className="Checkbox-title">
         <input type="checkbox" name={name} />
-        {title}
+        {name}
       </label>
     </div>
   );
