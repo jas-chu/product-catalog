62a8c41e3084af5c11c4de0c29db61c8a952e651
Jasmina Sella Faena <jasminasf@gmail.com>
2022-09-12 22:12:09 +0200

    refactor basic details tab

--- src/components/tabs/BasicDetailsTab.js
+++ src/components/tabs/BasicDetailsTab.js
@@ -1,39 +1,25 @@
+import React from 'react';
+import ProductInfo from '../ProductInfo';
+import StoreLinks from '../StoreLinks';
 import './BasicDetailsTab.css';
 
-const BasicDetailsTab = () => (
-  <div className="Basic-details-container">
-    <Header />
-    <ProductInfo />
-    <StoreLinks />
-  </div>
-);
+function BasicDetailsTab() {
+  return (
+    <div className="Basic-details-container">
+      <Header />
+      <ProductInfo />
+      <StoreLinks />
+    </div>
+  );
+}
 
-const Header = () => (
-  <div>
-    <p className="Title">Basic Product Details</p>
-    <p className="Subtitle">This is the large main article featured at the top of the page</p>
-  </div>
-);
-
-const ProductInfo = () => (
-  <div className="Section">
-    <p className="Section-title">Product Info</p>
-  </div>
-);
-
-const StoreLinks = () => (
-  <div className="Section">
-    <p className="Section-title">Store Links</p>
-    <p> Here goes the tabs </p>
-    <p> Select store inside the tabs</p>
-    <SelectStore />
-  </div>
-);
-
-const SelectStore = () => (
-  <div className="Section">
-    <p>Todo en row: select, input, button, checkbox</p>
-  </div>
-);
+function Header() {
+  return (
+    <div>
+      <p className="Title">Basic Product Details</p>
+      <p className="Subtitle">This is the large main article featured at the top of the page</p>
+    </div>
+  );
+}
 
 export default BasicDetailsTab;
