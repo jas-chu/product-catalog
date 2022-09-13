0402df839db18fbfce51ed088993f7417f3f7af6
Jasmina Sella Faena <jasminasf@gmail.com>
2022-09-13 16:21:21 +0200

    decoupling Header

--- src/components/tabs/BasicDetailsTab.js
+++ src/components/tabs/BasicDetailsTab.js
@@ -2,22 +2,14 @@ import React from 'react';
 import ProductInfo from '../ProductInfo';
 import StoreLinks from '../StoreLinks';
 import './BasicDetailsTab.css';
+import Header from './Header';
 
 function BasicDetailsTab() {
   return (
     <div className="Basic-details-container">
-      <Header />
+      <Header title="Basic Product Details" subtitle="This is the large main article featured at the top of the page" />
       <ProductInfo />
       <StoreLinks />
-    </div>
-  );
-}
-
-function Header() {
-  return (
-    <div>
-      <p className="Title">Basic Product Details</p>
-      <p className="Subtitle">This is the large main article featured at the top of the page</p>
     </div>
   );
 }
---
+++ src/components/tabs/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header({title, subtitle}) {
+  return (
+    <div>
+      <p className="Title">{title}</p>
+      <p className="Subtitle">{subtitle}</p>
+    </div>
+  );
+}
+
+export default Header;
