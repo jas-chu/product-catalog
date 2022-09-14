2194a6e21f1feda6ebe652a0d7a83fc3e62ce3b5
Jasmina Sella Faena <jasminasf@gmail.com>
2022-09-15 00:37:45 +0200

    adding drag and drop component

--- src/components/UploadImages.js
+++ src/components/UploadImages.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
+import ActionButton from './actionButton';
+import icon from '../assets/icon-image.png';
+
+const fileTypes = ['JPEG', 'PNG', 'GIF'];
 
 function EditImages() {
+  const [file, setFile] = useState(null);
+
+  const handleChange = (newFile) => {
+    // TODO: handle uploading multiple times.
+    setFile(newFile);
+  };
+
   return (
-    <div>
-      <p> upload images files</p>
+    <div className="File-uploader">
+      <FileUploader multiple handleChange={handleChange} name="file" types={fileTypes}>
+        <DragAndDrop />
+      </FileUploader>
+      <div>
+        <p>{file ? `File name: ${file[0].name}` : 'No images uploading'}</p>
+      </div>
+    </div>
+  );
+}
+
+function DragAndDrop() {
+  return (
+    <div className="Drag-and-drop">
+      <img src={icon} alt="upload" />
+      <b>Drag image to upload</b>
+      <p>- or -</p>
+      <ActionButton text="Choose Image" />
     </div>
   );
 }
